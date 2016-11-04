78e30b8d6af85a2f7cdd190cf8809e35c7f08925
oshai <ohadshai@gmail.com>
2016-11-04 23:45:00 +0200

    making the when expression more pretty

--- src/main/kotlin/oshai/World.kt
+++ src/main/kotlin/oshai/World.kt
@@ -1,5 +1,8 @@
 package oshai
 
+import oshai.CellState.Alive
+import oshai.CellState.Dead
+
 class World(initialWorld: List<List<CellState>>) {
     private var worldState: List<List<CellState>> = initialWorld
     private val X_SIZE = initialWorld.size
@@ -13,14 +16,14 @@ class World(initialWorld: List<List<CellState>>) {
     }
 
     private fun calcNewCell(x: Int, y: Int): CellState {
-        val currentCellState: CellState = worldState[x][y]
+        val state: CellState = worldState[x][y]
         val liveNeighbours: Int = calcLiveNeighbours(x, y)
         return when {
-            currentCellState == CellState.Alive && liveNeighbours < 2 -> CellState.Dead
-            currentCellState == CellState.Alive && liveNeighbours >= 2 && liveNeighbours <= 3 -> CellState.Alive
-            currentCellState == CellState.Alive && liveNeighbours > 3 -> CellState.Dead
-            currentCellState == CellState.Dead && liveNeighbours == 3 -> CellState.Alive
-            else -> CellState.Dead
+            state == Alive && liveNeighbours in 0..1    -> Dead
+            state == Alive && liveNeighbours in 2..3    -> Alive
+            state == Alive && liveNeighbours in 4..8    -> Dead
+            state == Dead && liveNeighbours == 3        -> Alive
+            else -> Dead
         }
     }
 
@@ -28,7 +31,7 @@ class World(initialWorld: List<List<CellState>>) {
         var live = 0
         for (i in Math.max(x - 1, 0)..Math.min(x + 1, X_SIZE - 1)) {
             for (j in Math.max(y - 1, 0)..Math.min(y + 1, Y_SIZE - 1)) {
-                if (worldState[i][j] == CellState.Alive && !(i == x && j == y)) {
+                if (worldState[i][j] == Alive && !(i == x && j == y)) {
                     live += 1
                 }
             }
